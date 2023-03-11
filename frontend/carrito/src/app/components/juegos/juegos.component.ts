6e141c0fdd37b980201444c86d4810b1e22492bf
Cinthia <votacinthia@gmail.com>
2023-03-11 00:39:05 -0300

    Maqueta Angular

---
+++ frontend/carrito/src/app/components/juegos/juegos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-juegos',
+  templateUrl: './juegos.component.html',
+  styleUrls: ['./juegos.component.css']
+})
+export class JuegosComponent implements OnInit {
+
+  constructor(private router : Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
